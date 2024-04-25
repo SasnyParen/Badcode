d358b5bdee7e4639631f22394a587647c7d663dd
Demit <83365420+SasnyParen@users.noreply.github.com>
2024-04-25 10:35:00 +0300

    Create badcode.java

---
+++ badcode.java
@@ -0,0 +1,25 @@
+public class BadCode {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a - b;
+        System.out.println("Сумма a и b: " + c);
+
+        String name = "John Doe";
+        int age = 30;
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Привет, мир!");
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+        // Различные другие операции и логика
+    }
+}
